1afdb9e49f2762383ad687857fdb736b76e7e40a
Serai Yan <serai@santori.ai>
2024-10-13 19:01:54 +0000

    2024 10 13 lesson

---
+++ 2024-09-22.py
@@ -0,0 +1,5 @@
+# every school has its own application
+# csu vs uc
+# https://admission.universityofcalifornia.edu/admission-requirements/freshman-requirements/california-residents/statewide-guarantee/
+# https://admission.universityofcalifornia.edu/how-to-apply/applying-as-a-freshman/
+# https://www.commonapp.org/plan/your-path-to-college
